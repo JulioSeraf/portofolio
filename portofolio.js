6a50f1c5101fbc879fd11c7673f44d875a0fd978
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-10-12 12:49:11 +0200

    creado menu humberger

--- portofolio.js
+++ portofolio.js
@@ -1,5 +1,5 @@
 const d = document,
-    $menuNav = d.querySelector('menu-nav')
+    $menuNav = d.querySelector('#menu-nav')
 d.addEventListener("DOMContentLoaded",(e)=>{
 
 });
@@ -7,6 +7,17 @@ d.addEventListener("click",(e)=>{
     // menu-nav
     if(e.target.matches('#icon-menu')){
         // alert('ok')
-        $menuNav.classList.toggle('no-active-menu')
-    }
-})
+        $menuNav.classList.toggle('no-active-menu');
+        d.getElementById('icon-menu').src = `imgs/icon-menu.png`
+        if($menuNav.classList.contains('no-active-menu'))d.getElementById('icon-menu').src = `imgs/cerrar.png`;;
+    };
+    if(e.target.matches("#menu-nav *") && innerWidth < 600){
+        $menuNav.classList.toggle('no-active-menu');
+        d.getElementById('icon-menu').src = `imgs/icon-menu.png`
+    };
+});
+// control de barra menu para desktop 
+window.addEventListener('resize',(e)=>{
+    if(innerWidth > 600) $menuNav.classList.add('no-active-menu');
+    if(innerWidth < 600) $menuNav.classList.remove('no-active-menu');
+});
