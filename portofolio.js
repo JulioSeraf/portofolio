3d303ebbc1e2762a709b506193b52536ebdc5d93
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-10-14 13:00:06 +0200

    criando section home

--- portofolio.js
+++ portofolio.js
@@ -1,23 +1,26 @@
 const d = document,
-    $menuNav = d.querySelector('#menu-nav')
-d.addEventListener("DOMContentLoaded",(e)=>{
-
+    $menuNav = d.querySelector('#menu-nav');
+    if(innerWidth > 600){
+        $menuNav.classList.add('active-menu');
+    };
+window.addEventListener('resize',()=>{
+    (innerWidth > 600)?$menuNav.classList.add('active-menu'):$menuNav.classList.remove('active-menu');
+    if($menuNav.classList.contains('active-menu'))d.getElementById('icon-menu').style.cssText = 'transform: rotate(0deg);'
 });
 d.addEventListener("click",(e)=>{
     // menu-nav
     if(e.target.matches('#icon-menu')){
         // alert('ok')
-        $menuNav.classList.toggle('no-active-menu');
-        d.getElementById('icon-menu').src = `imgs/icon-menu.png`
-        if($menuNav.classList.contains('no-active-menu'))d.getElementById('icon-menu').src = `imgs/cerrar.png`;;
+        $menuNav.style.cssText = `transition: 0.6s all ease-out;`
+        $menuNav.classList.toggle('active-menu');
+        d.getElementById('icon-menu').style.cssText = 'transform: rotate(0deg);';
+        if($menuNav.classList.contains('active-menu')) d.getElementById('icon-menu').style.cssText = 'transform: rotate(90deg);';
     };
     if(e.target.matches("#menu-nav *") && innerWidth < 600){
-        $menuNav.classList.toggle('no-active-menu');
-        d.getElementById('icon-menu').src = `imgs/icon-menu.png`
+        $menuNav.classList.toggle('active-menu');
+        d.getElementById('icon-menu').style.cssText = 'transform: rotate(0deg);';
     };
 });
+
 // control de barra menu para desktop 
-window.addEventListener('resize',(e)=>{
-    if(innerWidth > 600) $menuNav.classList.add('no-active-menu');
-    if(innerWidth < 600) $menuNav.classList.remove('no-active-menu');
-});
+
