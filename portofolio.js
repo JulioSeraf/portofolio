3d0fe0006d8cf5d9027922cc2b2aa24a42850bec
Julio Serafim <126347124+JulioSeraf@users.noreply.github.com>
2023-10-17 12:39:26 +0200

    criado btn de scroll

--- portofolio.js
+++ portofolio.js
@@ -1,8 +1,26 @@
 const d = document,
-    $menuNav = d.querySelector('#menu-nav');
-    if(innerWidth > 600){
-        $menuNav.classList.add('active-menu');
-    };
+    $menuNav = d.querySelector('#menu-nav'),
+    $scrollUp = d.getElementById('scroll-up'),
+    $darkLight = d.getElementById('dark-light');
+
+if(innerWidth > 600){
+    $menuNav.classList.add('active-menu');
+};
+
+    //scroll-up Displays
+ d.addEventListener('scroll',()=>{
+    if(scrollY >= 600){
+        $scrollUp.style.display = 'block'
+    }else{
+        $scrollUp.style.display = 'none';
+    };  
+});
+const themaDark = ()=>{
+
+}
+const themaLigth = ()=>{
+
+}
 window.addEventListener('resize',()=>{
     (innerWidth > 600)?$menuNav.classList.add('active-menu'):$menuNav.classList.remove('active-menu');
     if($menuNav.classList.contains('active-menu'))d.getElementById('icon-menu').style.cssText = 'transform: rotate(0deg);'
@@ -19,6 +37,17 @@ d.addEventListener("click",(e)=>{
     if(e.target.matches("#menu-nav *") && innerWidth < 600){
         $menuNav.classList.toggle('active-menu');
         d.getElementById('icon-menu').style.cssText = 'transform: rotate(0deg);';
+    };
+    //scroll para inicio
+    if(e.target.matches("#scroll-up *")){
+        scroll({
+            top:0,
+            behavior: "smooth"
+        });
+    };
+    if(e.target === $darkLight){
+        d.querySelector('body').classList.toggle('bodyDark');
+       
     };
 });
 
